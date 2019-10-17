2d6f2710a92380162d916a343e45886115808b6b
Huda Yousif <hjdoski@gmail.com>
2019-10-17 02:03:48 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,7 +10,12 @@ class EmailAddressParser
     @csv_emails = csv_emails
   end 
 
-
+  def parse 
+    csv_emails.split.collect do |address|
+      address.split(',') 
+    end
+    .flatten.uniq 
+  end
   
 end
   
